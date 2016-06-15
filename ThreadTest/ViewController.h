5aacc36539eee38fc742af2fa9e39d8931ae0efe
Lycodes_Dong <lycodes_dong@lycodesdemacbook-pro.local>
2016-06-15 15:42:25 +0800

    Initial Commit

---
+++ ThreadTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadTest
+//
+//  Created by Lycodes_Dong on 6/15/16.
+//  Copyright © 2016 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThreadTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThreadTest
+//
+//  Created by Lycodes_Dong on 6/15/16.
+//  Copyright © 2016 Dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
